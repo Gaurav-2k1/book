ad3620c268cce65018de8e33550ac4a8578405e7
ashwanikpankaj <ashnit8294@gmail.com>
2021-12-14 18:24:22 +0530

    booking heading page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { Book } from './components/Book';
 
 function App() {
   return (
     <div className="App">
     <h1>Book My Show</h1>
+    <Book/>
     </div>
   );
 }
